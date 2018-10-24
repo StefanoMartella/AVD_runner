30e97e8ddb14494f4bcb7288b4354abbd48b4308
StefanoMartella <stefano.martella9614@gmail.com>
2018-10-24 20:17:57 +0200

    Added more comments

--- AVD_runner.sh
+++ AVD_runner.sh
@@ -1,30 +1,41 @@
 #!/bin/bash
 
+# Setting variables:
 
-#######################
-#  Setting variables  #
-#######################
-
-# Put in LIST_AVD variable the path to the avdmanager script located inside */Android/Sdk/tools/bin
-# Include the extension(.bat) in Windows
+# Put in LIST_AVD variable the path to the
+# avdmanager script located inside */Android/Sdk/tools/bin/
+# Include the extension(.bat) in Windows.
 LIST_AVD="AVDMANAGER_PATH"
 
-# Put in RUN_AVD variable the path to the emulator script located inside */Android/Sdk/tools
-# Include the extension(.exe) in Windows
+# Put in RUN_AVD variable the path to the
+# emulator exe located inside */Android/sdk/emulator/
+# Include the extension(.exe) in Windows.
 RUN_AVD="EMULATOR_PATH"
 
+########################################################################
+#                                                                      #
+#     If you don't have JAVA_HOME, ANDROID_HOME(Windows and MacOS)     #
+#                                                                      #
+#     or ANDROID_AVD_HOME(Linux) in your PATH variable you need to     #
+#                                                                      #
+#            uncomment the following export instructions:              #
+#                                                                      #
+########################################################################
+
 # Put in JAVA_HOME variable the path to the Java SDK
-export JAVA_HOME="JAVA_JDK_PATH"
+#export JAVA_HOME="JAVA_JDK_PATH"
 
-# Put in ANDROID_HOME variable the path to the Android Studio SDK(for Windows and MacOS)
-export ANDROID_HOME="ANDROID_STUDIO_SDK_PATH"
-# Put in ANDROID_AVD_HOME variable the path to the Android Studio SDK(for Linux) .ini files of emulator
-export ANDROID_AVD_HOME="ANDROID_STUDIO_SDK_PATH_LINUX"
+# For Windows and MacOS:
+# Put in ANDROID_HOME variable the path to the Android Studio SDK
+#export ANDROID_HOME="ANDROID_STUDIO_SDK_PATH"
+
+# For Linux:
+# Put in ANDROID_AVD_HOME variable the path to the
+# Android Studio SDK) .ini files of emulator
+#export ANDROID_AVD_HOME="ANDROID_STUDIO_SDK_PATH_LINUX"
 
 
-#######################
-#     Script core     #
-#######################
+# Script core:
 
 printf "\n"
 
@@ -39,13 +50,13 @@ printf "\n"
 
 device_number=0
 
-# Save available devices name
+# Save available devices name.
 devices_name="$(echo "$devices_specs" | grep 'Name: ')"
 
 # Map of the type {device_number: device_name}
 devices_list=()
 
-# Put in a map the device number and its name
+# Put in a map the device number and its name.
 while read -r line
 do
   current_device="$(echo "$line" | sed -n -e 's/^.*Name: //p')"
@@ -56,13 +67,13 @@ done <<< "$devices_name"
 
 printf "\n"
 
-# Waiting for device choice
+# Waiting for device choice.
 read  -p "Choose a device and insert its number:" device_number_choice
 
-# Check if the device choice is valid
+# Check if the device choice is valid.
 while true
 do
-  # Check if the chosen device's number is in the map's keys of available devices
+  # Check if the chosen device's number is in the map's keys.
   if [[ -z "${devices_list[$device_number_choice]}" ]]; then
     read  -p "Invalid device number, try again:" device_number_choice
   else
@@ -70,5 +81,5 @@ do
   fi
 done
 
-# Run emulator according to the choice and let the bash in background
+# Run emulator according to the choice and let the bash in background.
 $RUN_AVD -avd "${devices_list[$device_number_choice]}" &
--- README.md
+++ README.md
@@ -4,36 +4,38 @@ This script is made to work with Linux, Windows and MacOS running bash >= 3.
 
 ## Prerequisites
  - Git bash(Windows)
- 
+
 ## Functionalities and Setup
 After running the script all available devices with their specs are displayed on the console:<br />
 each device is associated to a number; just enter the number of the device you want to run and the emulator will automatically start.
 <br />
 To run the script you need to set some variables editing the following lines:
 ```sh
-# Put in LIST_AVD variable the path to the avdmanager script located inside */Android/Sdk/tools/bin
-# Include the extension(.bat) in Windows
+# Put in LIST_AVD variable the path to the avdmanager script located inside */Android/Sdk/tools/bin/
+# Include the extension(.bat) in Windows.
 LIST_AVD="AVDMANAGER_PATH"
 
-# Put in RUN_AVD variable the path to the emulator script located inside */Android/Sdk/tools
-# Include the extension(.exe) in Windows
+# Put in RUN_AVD variable the path to the emulator exe located inside */Android/sdk/emulator/
+# Include the extension(.exe) in Windows.
 RUN_AVD="EMULATOR_PATH"
+```
 
+If you don't have JAVA_HOME, ANDROID_HOME(Windows and MacOS) or ANDROID_AVD_HOME(Linux)
+in your PATH variable you need to set the following variables:  
+
+```sh
 # Put in JAVA_HOME variable the path to the Java SDK
 export JAVA_HOME="JAVA_JDK_PATH"
 
-# Put in ANDROID_HOME variable the path to the Android Studio SDK(for Windows and MacOS)
+# For Windows and MacOS:
+# Put in ANDROID_HOME variable the path to the Android Studio SDK
 export ANDROID_HOME="ANDROID_STUDIO_SDK_PATH"
-# Put in ANDROID_AVD_HOME variable the path to the Android Studio SDK(for Linux) .ini files of emulator
+
+# For Linux:
+# Put in ANDROID_AVD_HOME variable the path to the
+# Android Studio SDK) .ini files of emulator
 export ANDROID_AVD_HOME="ANDROID_STUDIO_SDK_PATH_LINUX"
 ```
-
-Considering that:
-
-- <b>LIST_AVD</b> is the path to the avdmanager file(include extension in Windows, .bat),
-- <b>RUN_AVD</b> is the path to the emulator(include extension in Windows, .exe), 
-- <b>JAVA_HOME</b> is the path to the java SDK,
-- <b>ANDROID_HOME</b> or <b>ANDROID_AVD_HOME</b> is the path to the Android SDK.
 
 ## Run it
 On Mac and Linux grant executable permissions to the script with:
